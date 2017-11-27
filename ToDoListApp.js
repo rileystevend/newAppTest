afa7f8af9b59b8198ba399e83657f28b77bc39ac
Steven Riley <rileystevend@gmail.com>
2017-11-27 00:04:24 -0600

    v6

--- ToDoListApp.js
+++ ToDoListApp.js
@@ -1,14 +1,25 @@
 var toDo = {
   toDoList: [],
   displayToDoList: function () {
-    console.log('My To-dos', this.toDoList);
+    if (this.toDoList.length === 0) {
+      console.log('Your To-Do List is empty!');
+    } else {
+      console.log('My To-dos:');
+    for (var i = 0; i <= this.toDoList.length-1; i++) {
+      if (this.toDoList[i].completed === true) {
+        console.log('(x)'this.toDoList[i].item);
+      } else {
+        console.log('( )'this.toDoList[i].item);
+      }
+    }
+    }
   },
   addToDoList: function (itemName) {
     this.toDoList.push({
       item: itemName,
       completed: false
-    }),
-  }
+    })
+  },
   changeToDoList: function (),
   deleteToDoList: function (index) {
     this.toDoList.splice(index, 1)
@@ -17,5 +28,19 @@ var toDo = {
     var todo = this.toDoList[index];
     toDoList.completed = !todo.completed;
     this.displayToDoList();
+  },
+  toggleAll: function() {
+    var totalTodos = this.ToDoList.length;
+    var completedTodos = 0;
+    for (var i = 0; i < totalTodos; i++) {
+      if (this.ToDoList[i].completed === true){
+        completedTodos++;
+      }
     }
+    if (completedTodos === totalTodos) {
+      for (var i = 0; i < totalTodos; i++) {
+        this.toDoList[i].completed = false;
+      }
+    }
+  }
 }
